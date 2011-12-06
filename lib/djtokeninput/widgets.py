c90fc3b0268f55b3b5e05eb1490cb30b6ee7f20d
Adam Mckaig <adam.mckaig@gmail.com>
2011-12-06 11:49:21 -0500

    Allow Tokeninput settings to be specified by TokenWidget.
    Any kwarg (other than attrs) passed to TokenWidget.__init__ is assumed
    to be a Tokeninput setting, and is passed along to the client-side via
    a JSON-encoded data- attr. The keys are not checked for validity, to
    ensure future compatibility with new Tokeninput features.
    
    Settings can be specified using jQuery-style camelCase keys, or Python-
    style underscored_keys. I like to use underscores.
    
    A list of available settings can be found here:
    http://loopj.com/jquery-tokeninput/

--- lib/djtokeninput/widgets.py
+++ lib/djtokeninput/widgets.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import re
 import json
+import copy
 from django import forms
 from django.core.urlresolvers import reverse
 
@@ -16,12 +18,34 @@ class TokenWidget(forms.TextInput):
       "js/djtokeninput.js"
     )
 
+  def __init__(self, attrs=None, **kwargs):
+    super(TokenWidget, self).__init__(attrs)
+    self.settings = self._normalize(kwargs)
+
+  @classmethod
+  def _normalize(cls, settings):
+    """
+    Return a copy of `settings` (a dict) with any underscored keys replaced
+    with camelCased keys. Useful for passing Python-style dicts to Tokeninput,
+    which expects strange camelCase dicts.
+    """
+
+    return dict([
+      (cls._camelcase(key), val)
+      for key, val in settings.items()
+    ])
+
+  @staticmethod
+  def _camelcase(s):
+    return re.sub("_(.)", lambda m: m.group(1).capitalize(), s)
+
   @staticmethod
   def _class_name(value):
     return value.replace(" ", "-")
 
   def render(self, name, value, attrs=None):
     flat_value = ",".join(map(unicode, value or []))
+    settings = copy.copy(self.settings)
 
     url_name = getattr(self, "search_url", "djtokeninput_search")
     url_args = (self.model._meta.app_label, self.model._meta.object_name.lower())
@@ -31,11 +55,12 @@ class TokenWidget(forms.TextInput):
       attrs.get("class"), "tokeninput")
 
     if value is not None:
-      attrs["data-prepopulate"] = json.dumps([
+      settings["prePopulate"] = [
         {"id": pk, "name": unicode(self.choices.queryset.get(pk=pk))}
         for pk in value
-      ])
+      ]
 
+    attrs["data-settings"] = json.dumps(settings)
     return super(TokenWidget, self).render(name, flat_value, attrs)
 
   @staticmethod
