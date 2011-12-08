7043c0a4edd14dc22068111202e1408220a805b4
Adam Mckaig <adam.mckaig@gmail.com>
2011-12-08 16:22:05 -0500

    Add TokenWidget usage to example app.

--- example/app/forms.py
+++ example/app/forms.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 from django import forms
-from djtokeninput.fields import TokenField
+from djtokeninput.fields import TokenField, TokenWidget
 from app import models
 
 
 class ExampleForm(forms.Form):
   title = forms.CharField()
   desc = forms.CharField(widget=forms.Textarea)
-  tags = TokenField(models.Tag, required=False)
+  tags = TokenField(models.Tag, required=False,
+    widget=TokenWidget(prevent_duplicates=True))
